c5456d6f4174353c5a5a3e44dba82cea52fe8c24
Joseph LaChance <joeyasaperson@gmail.com>
2025-08-11 12:31:31 -0500

    I dont know anymore. I guess Im cleaning things up. But also trying to figure out how I want this game to work? So a lot of banging my head agaisnt the wall and trying to not get discouraged.

--- Game.py
+++ Game.py
@@ -3,7 +3,7 @@ import time
 
 valid_signs=["sun", "moon", "star"] #usable signs
 
-
+#===CLASSES AND FUNCTIONS=== 
 class Player:
     def __init__(self, name, sign):
         self.name = name
@@ -26,69 +26,61 @@ class Player:
             self.luck += 5
 
 class Scene:
-    def __init__(self, description, choices, look_description):
+    def __init__(self, description, choices, look_description, actions):
         self.description = description
         self.choices = choices
         self.look_description = look_description
+        self.actions = actions or {} 
 
 
-    def enter(self):
+    def enter(self, player):
         print(self.description)
         while True:
-            action = input("\nWhat do you want to do?")
+            action = input("\nWhat do you want to do? ")
             if "look" in action:
                 print(self.look_description)
             elif "inventory" in action:
-                print (Player.inventory)
+                print (player.inventory)
                 break
             else:
-                print("Nah mang")
+                print("Try 'looking' around or checking on your 'inventory' ")
 
 
 
 def enter():
     input("Press Enter to continue... ")
 
-def choice_yn(prompt):
-    #Prompt means that 
-    while True:
-        answer = input(prompt + " Yes or No? ").lower()
-        if answer in ["yes","y"]:
-            return True
-        elif answer in ["no","n"]:
-            return False
-        else:
-            print("Please answer Yes or No")
-
 def intro():
     print("Welcome to the game. Joseph LaChance made this game. ")
     input("Press Enter to continue...")
     print("Lets get started.")
     name = input("What is your name? ")
-    print(f"Welcome {name} You must now think long and hard about the next question.")
-    sign = input("What is your sign? The of the Sun, That of the Moon, Or that of the Star?").lower()
+    print(f"Welcome >{name}< You must now think long and hard about the next question.")
+    sign = input("What is your sign? That of the Sun, That of the Moon, Or that of the Star? ").lower()
     while sign not in valid_signs:
-        print("That is not a valid sign. Please choose from Sun, Moon, or Star")
+        print("That is not a valid sign. Please choose from Sun, Moon, or Star ")
         sign = input("What is your sign? ").lower()
     print(f"You have chosen {sign}! ")
     
     enter()
     print(f"\nNow. {name}. Your journey begins at dawn. Get some rest")
     enter()
-
+    return Player(name, sign)
 
 tavern = Scene(
         description="You are in a warm bustling tavern",
-        choices=[],
+        choices=["lady", "bartender", "patrons", "hood",],
         look_description="""
-        There a number of patrons enjoying their drinks and talking amongst them.
+        There a number of patrons enjoying their drinks and talking amongst themselves.
         A woman is sitting alone however, covered in a dark hood that resembles a clan
         that you've heard of, but not entirely sure.
         
-        The bartender is looking at you now as check the surroundings. What would like to do?
-        """
-    )
-
-#GAME BEGINS
-intro()
-tavern.enter()
+        The bartender is looking at you now as check the surroundings.What would like to do?
+        """, 
+        actions={
+            "lady": print
+        }
+)
+#===GAME BEGINS===
+player = intro()
+tavern.enter(player)
